20f11c02b52d5f5ca468b74c7e750f932e3a8c4b
Hansana Madhuranga <153816119+Hansana0613@users.noreply.github.com>
2024-12-30 10:59:42 +0530

    use SQLite to store data in local database in android
    configure SQLite

--- app/src/main/java/lk/javainstitute/app16/MainActivity.java
+++ app/src/main/java/lk/javainstitute/app16/MainActivity.java
@@ -1,8 +1,14 @@
 package lk.javainstitute.app16;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -20,5 +26,32 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                A a = new A(MainActivity.this, "app16.db", null, 1);
+
+            }
+        });
+    }
+}
+
+class A extends SQLiteOpenHelper {
+
+    public A(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL("");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
     }
 }
